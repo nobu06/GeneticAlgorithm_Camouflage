6f1cf5eac79f6c660a9461aeae5e945db4516a21
nobu06 <nobu.rs06@gmail.com>
2018-08-30 15:47:48 +0900

    delete unnecessary comments in scripts

--- Assets/DNA.cs
+++ Assets/DNA.cs
@@ -1,5 +1,6 @@
 ﻿/*
  * Ones that survive on the screen the longest are the fittest
+ * https://www.udemy.com/machine-learning-with-unity/
  */
 
 using System.Collections;
@@ -15,7 +16,7 @@ public class DNA : MonoBehaviour
     public float b;
 
     // size
-    public float scaleX;     // Note: if have two vals for scale - scaleX and scaleY, can make people with different body proportions too
+    public float scaleX;     // Note:  with two values for x and y, can make people with different body proportions
     public float scaleY;
 
     bool dead = false;              // dead when clicked on
@@ -47,50 +48,3 @@ public class DNA : MonoBehaviour
 
     }
 }
-
-
-
-
-
-///*
-// * Ones that survive on the screen the longest are the fittest
-// */
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class DNA : MonoBehaviour {
-
-//    // gene for color that will be passed to the offsprings
-//    public float r;
-//    public float g;
-//    public float b;
-
-//    bool dead = false;              // dead when clicked on
-//    public float timeToDie = 0f;    // time until they get clicked
-//    SpriteRenderer sRenderer;  // to turn them off
-//    Collider2D sCollider;           // to turn them off
-
-//	private void OnMouseDown()
-//	{
-//        dead = true;
-//        timeToDie = PopulationManager.elapsed;
-//        Debug.Log("Dead At: " + timeToDie);
-//        sRenderer.enabled = false;
-//        sCollider.enabled = false;
-//	}
-
-//	void Start ()
-//    {
-//        sRenderer = GetComponent<SpriteRenderer>();
-//        sCollider = GetComponent<Collider2D>();
-
-//        sRenderer.color = new Color(r, g, b);
-//	}
-	
-//	void Update ()
-//    {
-		
-//	}
-//}
